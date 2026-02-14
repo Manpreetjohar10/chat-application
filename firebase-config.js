c50f7a50758b8da2c22ad161e6ff2eb13f3623e2
manpreet <manpreetjohar49@gmail.com>
2026-02-14 11:33:08 +0530

    Track firebase-config.js for Netlify static deploy

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  "apiKey": "AIzaSyDmf_yh2tse2JXCKnBc0EHlPCfPnumfboE",
+  "authDomain": "chat-application-fb73d.firebaseapp.com",
+  "databaseURL": "https://chat-application-fb73d-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  "projectId": "chat-application-fb73d",
+  "storageBucket": "chat-application-fb73d.firebasestorage.app",
+  "messagingSenderId": "871947458329",
+  "appId": "1:871947458329:web:5bd50fee6f939052a3d788"
+};
